20f58d709e3b40c6075544bd6aa4682dbbf9bfca
Don Price <askdonno@gmail.com>
2015-07-27 20:32:12 -0400

    clean up code

--- result.php
+++ result.php
@@ -9,22 +9,10 @@
 	</head>
     <body>
 
-        <form action="" method="post">
-			Régions: <input name="region" id="regionsearch" type="text" />
+        <form method="post" action="result.php">
+			       Régions: <input name="regionsearch" id="regionsearch" type="text" />
+             <button class="btn" type="submit">Search</button>
         </form>
-
-			<!-- JAVASCRIPT -->
-			<script type="text/javascript" src="js/jquery-1.11.3.js"></script>
-			<script type="text/javascript" src="js/jquery-ui.js"></script>
-			<!-- JQUERY AUTOCOMPLETE OPTIONS -->
-			<script type="text/javascript">
-				jQuery(document).ready(function(){
-					$('#regionsearch').autocomplete({
-					source : 'search.php',
-					minLength : 3,
-					});
-				});
-			</script>
 
       <?php
 
@@ -35,7 +23,9 @@
           echo "<table>";
             echo "<thead>";
               echo "<tr>";
-                echo "<th>";
+                echo "<th>population</th>";
+                echo "<th>City</th>";
+                echo "<th>Slug</th>";
               echo "</tr>";
             echo "</thead>";
             echo "<tbody>";
@@ -43,24 +33,36 @@
         // VARIABLE FOR SEARCH TERM
         $regionsearch = $_POST['regionsearch'];
         // USER QUERY
-        $query = $dv->prepare("SELECT * FROM population WHERE location LIKE :regionsearch");
+        $query = $db->prepare("SELECT * FROM population WHERE location LIKE :regionsearch");
         // BIND PARAMETERS
         $query->bindParam(':regionsearch', $regionsearch);
         // EXECUTE STATEMENT
         $query->execute();
         // SEARCH DATABASE AND DISPLAY INFO
         $queryResult = $query->fetchAll();
-        $resultArray = array();
         foreach ($queryResult as $row) {
           echo"<tr>";
-        echo"<td>".$row[0]."</td>";
-    echo"<td>".$row[1]."</td>";
-    echo"<td>".$row[2]."</td>";
-        echo"</tr>";
+            echo"<td>".$row[0]."</td>";
+            echo"<td>".$row[1]."</td>";
+            echo"<td>".$row[2]."</td>";
+          echo"</tr>";
           }
           echo "</tbody>";
-            echo "</table>";
+          echo "</table>";
         }
        ?>
+
+       <!-- JAVASCRIPT -->
+ 			<script type="text/javascript" src="js/jquery-1.11.3.js"></script>
+ 			<script type="text/javascript" src="js/jquery-ui.js"></script>
+ 			<!-- JQUERY AUTOCOMPLETE OPTIONS -->
+ 			<script type="text/javascript">
+ 				jQuery(document).ready(function(){
+ 					$('#regionsearch').autocomplete({
+ 					source : 'search.php',
+ 					minLength : 3,
+ 					});
+ 				});
+ 			</script>
    	</body>
 </html>
